fccc7b1833e959a04341d04994fb10cec19e4582
신현만 <cswsshm0627@gmail.com>
2018-10-29 21:32:32 +0900

    Initial commit

---
+++ library/src/main/java/com/hm/library/TestClass.java
@@ -0,0 +1,10 @@
+package com.hm.library;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class TestClass {
+    public void mm(Context context, String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+    }
+}
